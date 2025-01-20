c6fa8e4e0aef2f6d68ab8dfb2e334cf40b7424ed
Tarin Pratt <tarinpratt@gmail.com>
2025-01-20 12:44:26 -0700

    changed db creds in config file

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', 'u569634487_jw4t4' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u569634487_CwfXW' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'Flauncher9000#' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
