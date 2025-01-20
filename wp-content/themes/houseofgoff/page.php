10521ba137de97d67200e4b11a620f04edcf860f
Tarin Pratt <tarinpratt@gmail.com>
2025-01-20 12:16:20 -0700

    changed and h2 tag

--- wp-content/themes/houseofgoff/page.php
+++ wp-content/themes/houseofgoff/page.php
@@ -3,6 +3,7 @@
         the_post(); ?>
         <h1><?= the_title(); ?></h1>
         <!-- just testing changes -->
+        <h2>Here are the changes</h2>
         <?php the_content(); ?>
 <?php }
 get_footer();
