5dbcbd4582f083704f288899e73d751ba8c17430
Tarin Pratt <tarinpratt@gmail.com>
2025-01-20 12:45:52 -0700

    removed testing html from page.php

--- wp-content/themes/houseofgoff/page.php
+++ wp-content/themes/houseofgoff/page.php
@@ -2,8 +2,6 @@
     while(have_posts()) {
         the_post(); ?>
         <h1><?= the_title(); ?></h1>
-        <!-- just testing changes -->
-        <h2>Here are the changes</h2>
         <?php the_content(); ?>
 <?php }
 get_footer();
