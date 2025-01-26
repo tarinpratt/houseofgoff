d19e8aa55b9a0d4b73bdfaf3965e0b97b472d3b8
Tarin Pratt <tarinpratt@gmail.com>
2025-01-26 10:58:57 -0700

    adding placeholder copy

--- wp-content/themes/houseofgoff/page.php
+++ wp-content/themes/houseofgoff/page.php
@@ -1,7 +1,6 @@
 <?php get_header();
     while(have_posts()) {
         the_post(); ?>
-        <h1><?= the_title(); ?></h1>
         <div class="featured_img" style="background-image: url('<?=the_post_thumbnail_url();?>');"></div>        
         <?php 
         the_content(); 
