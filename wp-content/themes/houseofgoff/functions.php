0f51641d3c62c6ea37b2df413cbf7ddc878672d8
Tarin Pratt <tarinpratt@gmail.com>
2025-01-26 13:22:33 -0700

    adding js file

--- wp-content/themes/houseofgoff/functions.php
+++ wp-content/themes/houseofgoff/functions.php
@@ -2,6 +2,11 @@
     function houseofgoff_files() {
         wp_enqueue_style('hosueofgoff_styles', get_stylesheet_uri());
     }
+    function custom_scripts() {
+        wp_enqueue_script( 'custom-script', get_template_directory_uri() . '/js/custom.js', array(), '1.0', true );
+    }
+    
+    add_action( 'wp_enqueue_scripts', 'custom_scripts' );
  
     add_action('wp_enqueue_scripts', 'houseofgoff_files');
 
