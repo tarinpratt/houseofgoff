5cfca208cb86e5ae1523bd6b66c328452787447d
Tarin Pratt <tarinpratt@gmail.com>
2025-05-25 13:24:26 -0700

    general css edits for rsvp page

--- wp-content/themes/houseofgoff/js/custom.js
+++ wp-content/themes/houseofgoff/js/custom.js
@@ -2,3 +2,9 @@
 setTimeout(() => {
     console.log("JAVASCRIPT IS WORKING with setttimeout function");
 }, 10000);
+$(function() {
+    $('#hamburger').on('click', function() {
+      $(this).toggleClass('close');
+      $('nav').toggleClass('visible');
+    });
+  });
