86b273ea8947c59de23f7ee972834d71ad56d75d
Tarin Pratt <tarinpratt@gmail.com>
2025-05-25 16:54:04 -0700

    general css edits for rsvp page

--- wp-content/themes/houseofgoff/functions.php
+++ wp-content/themes/houseofgoff/functions.php
@@ -5,7 +5,7 @@
     function custom_scripts() {
         wp_enqueue_script( 'custom-script', get_template_directory_uri() . '/js/custom.js', array(), '1.0', true );
     }
-    
+    add_action('wp_enqueue_scripts', 'my_custom_scripts');
     add_action( 'wp_enqueue_scripts', 'custom_scripts' );
  
     add_action('wp_enqueue_scripts', 'houseofgoff_files');
