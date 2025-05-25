aa3cd2ce3a7cae538ebf0aaeefc07202086a659f
Tarin Pratt <tarinpratt@gmail.com>
2025-05-25 16:37:14 -0700

    general css edits for rsvp page

--- wp-content/themes/houseofgoff/header.php
+++ wp-content/themes/houseofgoff/header.php
@@ -10,7 +10,6 @@
         <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Mona+Sans:ital,wght@0,200..900;1,200..900&family=Pinyon+Script&display=swap" rel="stylesheet">
         <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <script src="https://www.google.com/recaptcha/enterprise.js?render=6LcO_EgrAAAAAH1rxRD7xt-q1mUA6uOeKjJDHbcQ"></script>
         <?php wp_head(); ?>
         
     </head>
