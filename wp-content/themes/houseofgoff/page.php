7af0745721bf244e31ccb2911e13d5285ae96f4c
Tarin Pratt <tarinpratt@gmail.com>
2025-05-25 08:39:39 -0700

    general css edits for rsvp page

--- wp-content/themes/houseofgoff/page.php
+++ wp-content/themes/houseofgoff/page.php
@@ -2,7 +2,7 @@
     while(have_posts()) {
         the_post(); ?>
         <div class="main">
-        <div class="featured_img" style="background-image: url('<?=the_post_thumbnail_url();?>');" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500"></div>        
+        <div class="featured_img" style="background-image: url('<?=the_post_thumbnail_url();?>');"></div>        
         <?php 
         the_content(); 
         ?>
