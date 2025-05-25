21305834646b674f68075444be61ed094bc2ed2a
Tarin Pratt <tarinpratt@gmail.com>
2025-05-25 14:24:15 -0700

    general css edits for rsvp page

--- wp-content/themes/houseofgoff/header.php
+++ wp-content/themes/houseofgoff/header.php
@@ -11,7 +11,7 @@
         <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-        <script src="https://www.google.com/recaptcha/enterprise.js?render=6Lfy-EgrAAAAAAl19WcXQ3woyz913c_Zz43--sgc"></script>
+        <script src="https://www.google.com/recaptcha/enterprise.js?render=6LcO_EgrAAAAAH1rxRD7xt-q1mUA6uOeKjJDHbcQ"></script>
         <?php wp_head(); ?>
         
     </head>
