c21ea366dd845cf19ef33e71980ca8f689460d1a
Tarin Pratt <tarinpratt@gmail.com>
2025-05-25 18:53:38 -0700

    general css edits for rsvp page

--- wp-content/themes/houseofgoff/footer.php
+++ wp-content/themes/houseofgoff/footer.php
@@ -1,4 +1,7 @@
 <?php wp_footer(); ?>
+<footer>
+    <p class="">"Website designed and developed by: Tarin Pratt</p><img src="/wp-content/uploads/2025/05/tlplogo.png" alt="t l p logo" />
+</footer>
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="/js/custom.js" type="text/javascript"></script>
